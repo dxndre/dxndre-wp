84c6e673f5c9c8580ee33de3c43c3361708528fe
D'André Phillips <d.xndre@outlook.com>
2026-01-22 18:52:19 +0000

    More responsiver changes to the modal

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '2e6b5641c692ed7753d8');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '6731835e1350f3f590f8');
