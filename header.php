3b2e70361eb35d37600c9dec704cff0f37d76c65
D'André Phillips <d.xndre@outlook.com>
2026-01-22 17:17:36 +0000

    Removed GTM (Installed plugin instead)

--- header.php
+++ header.php
@@ -4,14 +4,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-57MXGX3');</script>
-	<!-- End Google Tag Manager -->
-
 	<?php wp_head(); ?>
 </head>
 
@@ -23,10 +15,6 @@
 ?>
 
 <body <?php body_class(); ?>>
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57MXGX3"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
 
 <?php wp_body_open(); ?>
 
