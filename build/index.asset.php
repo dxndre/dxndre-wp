5e8023a49d68c6bd1f1b470a2c64d221bf66d590
D'André Phillips <d.xndre@outlook.com>
2026-01-22 14:26:29 +0000

    Made the modal responsive.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'c5bcb200583681452d04');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '5f1866fa12aca4bbd727');
