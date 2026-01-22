9a737f53ac1c1134489315b0d8c76a741332f024
D'André Phillips <d.xndre@outlook.com>
2026-01-22 17:04:25 +0000

    Google Tag Manager

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '5f1866fa12aca4bbd727');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '2e6b5641c692ed7753d8');
